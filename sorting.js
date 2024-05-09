dba0c5f6f5f0f911851927f65725e2d1f58810d7
AmrasHameed <amrazhameed@gmail.com>
2024-05-09 11:24:31 +0530

    Queue Done

--- sorting.js
+++ sorting.js
@@ -12,46 +12,6 @@
 //     return arr
 // }
 
-
-
-// function bubbleSort(arr){
-//     swapped = false;
-//     for(let i = 0; i < arr.length -1;i++){
-//         if(arr[i] > arr[i+1]){
-//             [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
-//             swapped = true
-//         }
-//     }
-//     if(!swapped){
-//         return arr;
-//     } else {
-//         return bubbleSort(arr)
-//     }
-// }
-
-// function bubbleSort(arr) {
-//     for (let i = 0; i < arr.length - 1 ; i++) {
-//         for (let j = i+1; j < arr.length ; j++) {
-//             if(arr[j] < arr[i]){
-//                 [arr[j], arr[i]] = [arr[i], arr[j]]
-//             }
-//         }
-//     }
-// }
-
-// function insertionSort(arr) {
-//     for(let i = 1; i < arr.length; i++) {
-//         let numberToInsert = arr[i]
-//         let j = i - 1
-//         while(j >= 0 && arr[j] > numberToInsert) {
-//             arr[j+1] = arr[j]
-//             j = j-1
-//         }
-//         arr[j+1] = numberToInsert
-//     }
-// }
-
-
 // function insertionSort(arr) {
 //     for(let i = 1; i < arr.length; i++) {
 //         let num = arr[i]
@@ -65,41 +25,10 @@
 // }
 
 
-
-// function insertionSort(arr, i = arr.length) {
-//     if(i <= 1) {
-//         return
-//     }
-//     insertionSort(arr, i - 1)
-//     let num = arr[i - 1]
-//     let j = i - 2
-//     while(j >= 0 && arr[j] > num) {
-//         arr[j + 1] = arr[j]
-//         j = j - 1
-//     } 
-//     arr[j + 1] = num
-// }
-
-
-
-// function insertionSort(arr, i = arr.length) {
-//     if(i <= 0) {
-//         return 
-//     }
-//     insertionSort(arr, i - 1)
-//     let num = arr[i - 1]
-//     let j = i - 2
-//     while(j >= 0 && arr[j] > num) {
-//         arr[j + 1] = arr[j]
-//         j--
-//     }
-//     arr[j + 1] = num
-// }
-
 // function bubbleSort(arr) {
 //     let swap = false;
 //     for(let i = 0; i < arr.length - 1; i++) {
-//         if(arr[i] < arr[i + 1]) {
+//         if(arr[i] > arr[i + 1]) {
 //             [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
 //             swap = true
 //         }
@@ -122,6 +51,7 @@
 //         }
 //         [arr[i], arr[min]] = [arr[min], arr[i]]
 //     }
+//     return arr
 // }
 
 
@@ -137,47 +67,30 @@
 //             left.push(arr[i])
 //         } else {
 //             right.push(arr[i])
-//         }
+//         }    
 //     }
 //     return [...quickSort(left), pivot, ...quickSort(right)]
 // }
 
-// function quickSort(arr) {
+// function mergeSort(arr) {
 //     if(arr.length < 2) {
 //         return arr
 //     }
-//     let pivot = arr[arr.length - 1]
-//     let left = []
-//     let right = []
-//     for(let i = 0; i < arr.length - 1; i++) {
-//         if(arr[i] < pivot) {
-//             left.push(arr[i])
+//     let mid = Math.floor(arr.length / 2)
+//     let left  = arr.slice(0, mid)
+//     let right = arr.slice(mid)
+//     return merge(mergeSort(left), mergeSort(right))
+// }
+// function merge(left, right) {
+//     let sorted = []
+//     while(left.length && right.length) {
+//         if(left[0] <= right[0]) {
+//             sorted.push(left.shift())
 //         } else {
-//             right.push(arr[i])
+//             sorted.push(right.shift())
 //         }
 //     }
-//     return [...quickSort(left), pivot, ...quickSort(right)]
+//     return [...sorted, ...left, ...right]
 // }
-
-function mergeSort(arr) {
-    if(arr.length < 2) {
-        return arr
-    }
-    const mid = Math.floor(arr.length / 2)
-    const left = arr.slice(0, mid)
-    const right = arr.slice(mid)
-    return merge(mergeSort(left), mergeSort(right))
-}
-function merge(left, right) {
-    const sortedArr = []
-    while(left.length && right.length) {
-        if(left[0] <= right[0]) {
-            sortedArr.push(left.shift())
-        } else {
-            sortedArr.push(right.shift())
-        }
-    }
-    return [...sortedArr, ...left, ...right]
-}
 let arr = [5, -2, 8, -1, 9, 3, -7, 4, 10, -6]
-console.log(mergeSort(arr));
+console.log(quickSort(arr));
--- test.js
+++ test.js
@@ -1,61 +1,238 @@
+// function bubbleSort(arr) {
+//     let swap;
+//     do {
+//         swap = false;
+//         for(let i = 0; i < arr.length - 1; i++) {
+//             if(arr[i] > arr[i + 1]) {
+//                 [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]] 
+//                 swap = true
+//             }
+//         }
+//     } while(swap)
+// }
+
+// function insertionSort(arr) {
+//     for(let i = 1; i < arr.length; i++) {
+//         let num = arr[i]
+//         j = i - 1
+//         while(j >= 0 && arr[j] > num) {
+//             arr[j + 1] = arr[j]
+//             j = j - 1
+//         }
+//         arr[j + 1] = num
+//     }
+// }
+
+// function selectionSort(arr) {
+//     for(let i = 0; i < arr.length - 1; i++) {
+//         let min = i
+//         for(let j = i + 1; j < arr.length; j++) {
+//             if(arr[j] < arr[min]) {
+//                 min = j
+//             }
+//         }
+//         [arr[i], arr[min]] = [arr[min], arr[i]]
+//     }
+// }
+
+// function quickSort(arr) {
+//     if(arr.length < 2) {
+//         return arr
+//     }
+//     let pivot = arr[arr.length - 1]
+//     let left = []
+//     let right = []
+//     for(let i = 0; i < arr.length - 1; i++) {
+//         if(arr[i] < pivot) {
+//             left.push(arr[i])
+//         } else {
+//             right.push(arr[i])
+//         }
+//     }
+//     return [...quickSort(left), pivot, ...quickSort(right)]
+// }
+
+
+// function mergeSort(arr) {
+//     if(arr.length < 2) {
+//         return arr
+//     }
+//     let mid = Math.floor(arr.length / 2)
+//     let left = arr.slice(0, mid)
+//     let right = arr.slice(mid)
+//     return merge(mergeSort(left), mergeSort(right))
+// }
+// function merge(left, right) {
+//     let sorted = []
+//     while(left.length && right.length) {
+//         if(left[0] < right[0]) {
+//             sorted.push(left.shift())
+//         } else {
+//             sorted.push(right.shift())
+//         }
+//     }
+//     return [...sorted, ...left, ...right]
+// }
+
+// let arr = [4, 1, -1, 0, -5, 10, 19, 2]
+
+// console.log(mergeSort(arr));
+
+
+// class Stack {
+//     constructor() {
+//         this.stack = {}
+//         this.size = 0
+//     }
+
+//     push(value) {
+//         this.stack[this.size] = value
+//         this.size++
+//     }
+
+//     pop() {
+//         delete this.stack[this.size - 1]
+//         this.size--
+//     }
+
+//     peek() {
+//         console.log(this.stack[this.size - 1]);
+//     }
+
+//     print() {
+//         console.log(this.stack);
+//     }
+// }
+
+// const stack = new Stack()
+// stack.print()
+// stack.push(9)
+// stack.push(0)
+// stack.push(13)
+// stack.push(2)
+// stack.push(7)
+// stack.print()
+// stack.peek()
+// stack.pop()
+// stack.print()
+
+
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value
         this.next = null
     }
 }
-class LinkedList {
+class Linkedlist {
     constructor() {
         this.head = null
         this.size = 0
     }
-    prepend(value) {
+    push(value) {
         const node = new Node(value)
-        if(this.size == 0) {
-            this.head = node
-        } else {
-            node.next = this.head
-            this.head = node
-        }
+        node.next = this.head
+        this.head = node
         this.size++
     }
-    sum() {
+    pop() {
         if(this.size == 0) {
             console.log("List is empty");
             return
-        } else {
-            let sum = 0
-            let curr = this.head
-            while(curr) {
-                sum+=curr.value
-                curr = curr.next
-            }
-            console.log(sum);
+        }
+        let temp = this.head
+        this.head = this.head.next
+        temp = null
+        this.size--
+    }
+
+    peek() {
+        console.log(this.head.value);
+        return
+    }
+
+    reverse() {
+        let stack = []
+        let curr = this.head
+        while(curr) {
+            stack.push(curr)
+            curr = curr.next
+        }
+        this.head = stack.pop()
+        curr = this.head
+        while(curr) {
+            curr.next = stack.pop()
+            curr = curr.next
         }
     }
+
     print() {
-        if(this.size == 0) {
-            console.log("List is empty");
+        if(this.size == 0){
+            console.log("List is Empty");
             return
-        } else {
-            let curr = this.head
-            let listValue = ""
-            while(curr) {
-                listValue+=`${curr.value} `
-                curr = curr.next
-            }
-            console.log(listValue);
         }
+        let curr = this.head
+        let listValue = ''
+        while(curr) {
+            listValue += `${curr.value} `
+            curr = curr.next
+        }
+        console.log(listValue);
     }
 }
 
-const list = new LinkedList
-
-list.prepend(10)
-list.prepend(11)
-list.prepend(14)
-list.prepend(15)
-list.prepend(16)
-
+const list = new Linkedlist()
 list.print()
-list.sum()
+list.push(10)
+list.push(0)
+list.push(2)
+list.push(18)
+list.push(-1)
+list.push(9)
+list.print()
+list.reverse()
+list.print()
+console.log(list.head);
+
+
+// class Queue {
+//     constructor() {
+//         this.queue = {}
+//         this.front = 0
+//         this.rear = 0
+//     }
+
+//     enqueue(value) {
+//         this.queue[this.rear] = value
+//         this.rear++
+//     }
+    
+//     dequeue() {
+//         delete this.queue[this.front]
+//         this.front++
+//     }
+
+//     isEmpty() {
+//         return this.rear - this.front == 0
+//     }
+
+//     peek() {
+//         console.log(this.queue[this.front]);
+//     }
+//     print() {
+//         console.log(this.queue);
+//     }
+// }
+
+// const queue = new Queue()
+// queue.print()
+// console.log(queue.isEmpty())
+// queue.enqueue(10)
+// queue.enqueue(9)
+// queue.enqueue(0)
+// queue.enqueue(4)
+// queue.enqueue(6)
+// queue.enqueue(1)
+// queue.print()
+// queue.dequeue()
+// queue.print()
+
